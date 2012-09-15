aec50fb64d0075c0d555a4b5da03778dd79c3177
Maverick <maverick.lee225@gmail.com>
2012-09-15 12:22:56 -0400

    add something to save wav files

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from pyo import *
 s = Server().boot()
+s.start()
 sf = SfPlayer("test.wav", loop=True, mul=.3)
+info = sndinfo("test.wav")
 harm = Harmonizer(sf, transpo=-5, winsize=0.05).out()
-s.gui(locals())
+rec = Record(harm, filename="altered.wav", fileformat=0, sampletype=1)
+clean = Clean_objects(info[1], rec)
+clean.start()
