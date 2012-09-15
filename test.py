82d8d9db2a11b0955a2dc9795bf188dd4333857f
Maverick <maverick.lee225@gmail.com>
2012-09-15 14:12:10 -0400

    make recording not real time, because that is bad

--- test.py
+++ test.py
@@ -2,18 +2,15 @@ from pyo import *
 import subprocess
 import time
 
-s = Server().boot()
-s.start()
-sf = SfPlayer("test.wav", loop=True, mul=.3)
-info = sndinfo("test.wav")
-harm = Harmonizer(sf, transpo=-5, winsize=0.05).out()
-rec = Record(harm, filename="saved-samples/altered.wav", fileformat=0, sampletype=1)
-clean = Clean_objects(info[1], rec)
-clean.start()
+s = Server(duplex=0, audio="offline").boot()
+dur = info = sndinfo("test.wav")[1]
+s.recordOptions(dur=dur, filename="saved-samples/altered.wav", fileformat=0, sampletype=1)
 
-# this is kindof a hack to prevent it from
-# converting to mp3 right away
-time.sleep(info[1])
+# processing
+sf = SfPlayer("test.wav", loop=True, mul=.3)
+harm = Harmonizer(sf, transpo=-5, winsize=0.05).out()
+
+s.start()
 
 # mp3 conversion
 wav = 'saved-samples/altered.wav'
