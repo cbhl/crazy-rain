a87fafd89aa373b730da58973ab22754b439cfc3
Maverick <maverick.lee225@gmail.com>
2012-09-15 01:30:35 -0400

    correct the audio file path

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from pyo import *
 s = Server().boot()
-sf = SfPlayer("/Users/mavericklee/Desktop/test.wav", loop=True, mul=.3)
+sf = SfPlayer("test.wav", loop=True, mul=.3)
 harm = Harmonizer(sf, transpo=-5, winsize=0.05).out()
 s.gui(locals())
